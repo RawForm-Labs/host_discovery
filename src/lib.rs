e3d72e196da9209747a813b806daeb4d7de1cce1
chrispaig3 <v0idmatr1x.0@gmail.com>
2024-11-25 12:18:05 -0600

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-#[cfg(target_os = "macos")]
-use std::process::Command;
 #[warn(missing_docs, missing_debug_implementations)]
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 use raw_cpuid::CpuId;
@@ -11,6 +9,8 @@ use rayon::prelude::*;
 use std::fs;
 #[cfg(target_os = "linux")]
 use std::path::Path;
+#[cfg(target_os = "macos")]
+use std::process::Command;
 use std::{
     env::consts::{ARCH, OS},
     thread,
@@ -57,8 +57,11 @@ impl<'o, 'a> std::fmt::Display for OSProfile<'o, 'a> {
     }
 }
 
-impl<Model, Cores> std::fmt::Display for Processor<Model, Cores> 
-where Model: std::fmt::Debug, Cores: std::fmt::Debug {
+impl<Model, Cores> std::fmt::Display for Processor<Model, Cores>
+where
+    Model: std::fmt::Debug,
+    Cores: std::fmt::Debug,
+{
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{:?}", self)
     }
@@ -153,20 +156,6 @@ impl<'o, 'a> OSProfile<'o, 'a> {
     }
 }
 
-/// Returns a `Processor` object containing the CPU model and logical core count  (x86 only)
-#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-pub fn x86_cpu() -> Processor<ProcessorBrandString, u32> {
-    let cpuid = CpuId::new();
-    let brand = cpuid.get_processor_brand_string().unwrap();
-    let cores = cpuid.get_processor_capacity_feature_info().unwrap();
-
-    let cpu = Processor {
-        model: brand,
-        cores: cores.maximum_logical_processors() as u32,
-    };
-    cpu
-}
-
 /// Returns a `Processor` object containing the CPU model and logical core count (macOS only)
 #[cfg(target_os = "macos")]
 pub fn macos_cpu() -> Processor<String, String> {
@@ -179,15 +168,29 @@ pub fn macos_cpu() -> Processor<String, String> {
         .arg("hw.logicalcpu")
         .output()
         .expect("Failed to execute sysctl command");
-   
+
     let encoded_cpu_output = String::from_utf8(cpu_output.stdout).unwrap();
     let encoded_core_output = String::from_utf8(core_output.stdout).unwrap();
-    
-    let model= encoded_cpu_output.split(": ").nth(1).unwrap().trim();
+
+    let model = encoded_cpu_output.split(": ").nth(1).unwrap().trim();
     let cores = encoded_core_output.split(": ").nth(1).unwrap().trim();
     let cpu = Processor {
         model: model.to_string(),
         cores: cores.to_string(),
+    };
+    cpu
+}
+
+/// Returns a `Processor` object containing the CPU model and logical core count  (x86 only)
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+pub fn x86_cpu() -> Processor<ProcessorBrandString, u32> {
+    let cpuid = CpuId::new();
+    let brand = cpuid.get_processor_brand_string().unwrap();
+    let cores = cpuid.get_processor_capacity_feature_info().unwrap();
+
+    let cpu = Processor {
+        model: brand,
+        cores: cores.maximum_logical_processors() as u32,
     };
     cpu
 }
