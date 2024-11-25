09569823e4a8ec73956fe552f3b4dfe805e43136
chrispaig3 <v0idmatr1x.0@gmail.com>
2024-11-25 11:52:41 -0600

    experimental mac hardware support

--- src/display.rs
+++ src/display.rs
@@ -1,15 +1,3 @@
-// // display_with_lifetimes: Sugar for implementing display on OSProfile
-// #[macro_export]
-// macro_rules! display_with_lifetimes {
-//     ($type:ident) => {
-//         impl<'o, 'a> std::fmt::Display for $type<'o, 'a> {
-//             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-//                 write!(f, "{:?}", self)
-//             }
-//         }
-//     };
-// }
-
 // display: Sugar for implementing the Display trait on types
 #[macro_export]
 macro_rules! display {
